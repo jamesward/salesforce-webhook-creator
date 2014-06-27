ca85106d752f169b2896a40ec68de928566ccb2e
James Ward <james@jamesward.org>
2014-06-26 18:39:24 -0600

    add form validation

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -27,6 +27,7 @@ require(["angular"], function(angular) {
             fetchWebhooks();
           }).
           error(function(data) {
+            $scope.working = false;
             $scope.errorMessage = data.error.message;
           });
       };
@@ -42,6 +43,7 @@ require(["angular"], function(angular) {
       };
 
       var initForm = function() {
+        $scope.working = false;
         $scope.errorMessage = "";
         $scope.name = "";
         $scope.sobject = null;
@@ -49,11 +51,7 @@ require(["angular"], function(angular) {
         $scope.url = "";
       };
 
-      fetchWebhooks();
-      fetchSobjects();
-      initForm();
-
-      $scope.createWebhook = function() {
+      var selectedEvents = function() {
         var selectedEvents = [];
         for (var event in $scope.events) {
           if($scope.events.hasOwnProperty(event)) {
@@ -62,8 +60,35 @@ require(["angular"], function(angular) {
             }
           }
         }
-        $scope.errorMessage = "";
-        createWebhook($scope.name, $scope.sobject, selectedEvents, $scope.url);
+        return selectedEvents;
+      };
+
+      fetchWebhooks();
+      fetchSobjects();
+      initForm();
+
+      $scope.createWebhook = function() {
+        if ($scope.webhookForm.name.$valid && $scope.webhookForm.sobject.$valid && selectedEvents().length > 0 && $scope.webhookForm.url.$valid) {
+          $scope.errorMessage = "";
+          $scope.working = true;
+          createWebhook($scope.name, $scope.sobject, selectedEvents(), $scope.url);
+        }
+        else {
+          var error = "";
+          if ($scope.webhookForm.name.$invalid) {
+            error += "Name must contain only letters.  ";
+          }
+          if ($scope.webhookForm.sobject.$invalid) {
+            error += "An SObject must be selected.  ";
+          }
+          if (selectedEvents().length < 1) {
+            error += "At least one event must be selected.  ";
+          }
+          if ($scope.webhookForm.url.$invalid) {
+            error += "A URL must be specified.  ";
+          }
+          $scope.errorMessage = error;
+        }
       };
     });
 
