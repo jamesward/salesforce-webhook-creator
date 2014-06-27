3bf384321d85547c87e60beef7887a92e2dec152
James Ward <james@jamesward.org>
2014-06-26 19:11:06 -0600

    require https

--- app/Global.scala
+++ app/Global.scala
@@ -1,4 +1,18 @@
-import play.api.mvc.WithFilters
+import play.api.http.HeaderNames
+import play.api.mvc._
 import play.filters.gzip.GzipFilter
+import scala.concurrent.ExecutionContext.Implicits.global
 
-object Global extends WithFilters(new GzipFilter())
+import scala.concurrent.Future
+
+object Global extends WithFilters(new GzipFilter(), OnlyHttpsFilter)
+
+object OnlyHttpsFilter extends Filter {
+  def apply(nextFilter: (RequestHeader) => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
+    nextFilter(requestHeader).map { result =>
+      requestHeader.headers.get(HeaderNames.X_FORWARDED_PROTO).filter(_ != "https").fold(result) { proto =>
+        Results.MovedPermanently("https://" + requestHeader.host + requestHeader.uri)
+      }
+    }
+  }
+}
