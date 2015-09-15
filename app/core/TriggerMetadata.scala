bf0b066a64a8c2bf6c87448307965cdac563bd4e
James Ward <james@jamesward.org>
2015-09-15 13:06:50 -0600

    name cannot have spaces

--- app/core/TriggerMetadata.scala
+++ app/core/TriggerMetadata.scala
@@ -6,5 +6,8 @@ import play.api.libs.json.Json
 case class TriggerMetadata(name: String, sobject: String, events: List[TriggerEvent], url: String)
 
 object TriggerMetadata {
-  implicit val format = Json.format[TriggerMetadata]
+  implicit val reads = Json.reads[TriggerMetadata].map { triggerMetadata =>
+    triggerMetadata.copy(name = triggerMetadata.name.replaceAllLiterally(" ", ""))
+  }
+  implicit val writes = Json.writes[TriggerMetadata]
 }
